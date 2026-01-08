2a0e6d1545dd540bff0c8af07e37844ee0bba48f
divine <divinemaina@gmail.com>
2026-01-08 11:36:59 +0200

    feat: display questions one by one and get user input

--- QuestionService.java
+++ QuestionService.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class QuestionService {
    Question[] questions = new Question[5];
+   String selection[]=new String[5];
 
    public QuestionService(){
 questions[0] = new Question(
@@ -54,10 +57,22 @@ questions[4] = new Question(
 
    }
 
-   public void displayQuestions(){
+   public void playQuiz(){
+      int i=0;
       for( Question q:questions){
 
-         System.out.println(q);
+         System.out.println("Question no :" + q.getId());
+         System.out.println(q.getQuestion());
+         System.out.println(q.getOpt1());
+         System.out.println(q.getOpt2());
+         System.out.println(q.getOpt3());
+         System.out.println(q.getOpt4());
+         Scanner sc =new Scanner(System.in);
+         selection[i] =sc.nextLine();
+         i++;
+      }
+      for(String s : selection){
+System.out.println(s);
       }
    }
 }
